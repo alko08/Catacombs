0e7ea6766897fc93403813a9e9b1d067135f627b
Bill <bill.xia.pa@gmail.com>
2022-04-10 19:32:51 -0400

    UI Tweaks 2

--- Catacombs/Assets/Scripts/SelectorGlowScript.cs
+++ Catacombs/Assets/Scripts/SelectorGlowScript.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SelectorGlowScript : MonoBehaviour
 {
     // Variables
     public GameObject myGlow;
     public inventoryScript inventory;
+    public bool mousingOver;
 
     // On start, grab inventory. 
     void Start()
     {
         myGlow.SetActive(false);
         inventory = GameObject.Find("EventSystem").GetComponent<inventoryScript>();
+        mousingOver = false;
     }
+
+    // void Update()
+    // {
+    //     if (mousingOver) {
+    //         Debug.Log("The cursor entered the selectable UI element.");
+    //     }
+    // }
     
     // While mousing over this object, activate glow. If click is detected,
     // open inventory. 
-    void OnMouseOver()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("MouseOver == true");
         myGlow.SetActive(true);
-
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            inventory.SelectToInventory();
-        }
     }
 
     // When cursor leaves, deactivate glow. 
